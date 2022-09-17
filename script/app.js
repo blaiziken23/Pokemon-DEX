23e30f6fd56a12d8172860a2d426a42e9b812b29
blaiziken23 <jayviee.berdin@gmail.com>
2022-09-17 14:47:40 +0800

    update

--- script/app.js
+++ script/app.js
@@ -231,7 +231,7 @@ const modalPokedex = async (pokemonName) => {
                 <div class="col-evolution col-sm-4">
                   ${ newPokemon(species1Data) }
                 </div>
-                <div class="col-evolution col-sm p-0 justify-content-end">
+                <div class="col-evolution svg col-sm p-0 justify-content-end">
                   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                   </svg>
